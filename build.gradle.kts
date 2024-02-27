f1e6510a8433cc54eeabe8d018721580b6a7077f
shorty <ungegrieftfirma@gmail.com>
2024-02-27 15:04:38 +0100

    update uniquepixels repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,11 +10,16 @@ version = "1.0.0"
 description = "Permission plugin for UniquePixels"
 
 java {
-  toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+  toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
 
 repositories {
-  mavenLocal()
+  maven("https://repo.uniquepixels.net/repository/minecraft") {
+    credentials {
+      username = "projectwizard"
+      password = System.getenv("UP_NEXUS_PASSWORD")
+    }
+  }
   mavenCentral()
 }
 
@@ -49,7 +54,7 @@ tasks {
 
     // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
     // See https://openjdk.java.net/jeps/247 for more information.
-    options.release.set(21)
+    options.release.set(17)
   }
   javadoc {
     options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
